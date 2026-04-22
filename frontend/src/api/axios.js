76c8c0c65d7d13f842418289f7d1cf02fad4fee7
durga9322 <khiredurga@gmail.com>
2026-04-22 08:13:10 +0530

    update backend URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bookswap-backend-odto.onrender.com/api",
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    config.headers["Authorization"] = "Bearer " + token;
-  }
+  if (token) config.headers.Authorization = "Bearer " + token;
   return config;
 });
 
